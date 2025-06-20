28915c1f58ed7f54803dd525bd02cd5fd56e799a
Lalit Kumar Verma <lalit2506verma@gmail.com>
2025-06-20 18:47:08 +0530

    add react router dom successfully

---
+++ src/router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import LoginPage from "@/pages/LoginPage";
+import HomePage from "@/pages/HomePage";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomePage />
+    },
+    {
+        path: '/login',
+        element: <LoginPage />,
+    },
+]);
